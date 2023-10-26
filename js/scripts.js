090ffb14bfe6ceea441b1b4ca2032a62cbaefff8
mejia-dev <aaron.mejia8484@gmail.com>
2023-10-26 13:18:19 -0700

    Add functionality to check if input matches a numeral. Split numeral to numeralsList. Create new 0 variable numberTotal

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function evalNumerals(input) {
+  if (!input.match(/^[ivxlcdm]+$/i)) {
+    return "Invalid Roman numerals detected. Please check your input and try again."
+  }
+  input = input.toLowerCase();
+  const numeralsList = input.split("");
+  let numberTotal = 0;
+
+  numeralsList.forEach((letter, location))
+
+  return numberTotal;
+
+}
