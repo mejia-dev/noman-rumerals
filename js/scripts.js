3d53d0bbe22120c99d1fcb9ebdc19b388393ad47
mejia-dev <aaron.mejia8484@gmail.com>
2023-10-26 14:51:31 -0700

    Add error checker. Add a test for any V,L,D numerals to ensure that they aren't between two matching characters. Error will show if there is an error

--- js/scripts.js
+++ js/scripts.js
@@ -5,22 +5,31 @@ function evalNumerals(input) {
   input = input.toLowerCase();
   const numeralsList = input.split("");
   let numberTotal = "";
+  let errorCounter = 0;
+  let recentErrorMessage = "";
 
-  numeralsList.forEach(function(letter, pos) {
-    if (["i","x","c","m"].includes(letter)) {
-      const ixcmValues = {i:1,x:10,c:100,m:1000};
+  numeralsList.forEach(function (letter, pos) {
+    if (["i", "x", "c", "m"].includes(letter)) {
+      // Test Line Below, remove later
+      const ixcmValues = { i: 1, x: 10, c: 100, m: 1000 };
+
       numberTotal += "This number is ixcm: " + pos + ". ";
-    } else if (["v","l","d"].includes(letter)) {
-      numberTotal += "This number is vld: " + pos + ". ";
-    } else {
-      return "Unknown error"
+    } else if (["v", "l", "d"].includes(letter)) {
+      const vldValues = { v: 5, l: 50, d: 500 };
+      let charBefore = numeralsList[pos - 1];
+      let charAfter = numeralsList[pos + 1];
+      if ((charAfter === charBefore) && (charBefore != undefined) && (charAfter != undefined)) {
+        errorCounter += 1;
+        recentErrorMessage = "Invalid input. Numeral " + letter.toUpperCase() +" should never be surrounded by two of the same numerals"
+      } else {
+        numberTotal += vldValues[letter];
+      }
     }
-
-
-
-    
-
   })
 
-  return numberTotal;
+  if (errorCounter === 0) {
+    return numberTotal;
+  } else {
+    return recentErrorMessage;
+  }
 }
